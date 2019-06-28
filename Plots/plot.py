3003b5b1c6f9c234eb102223404a0c82e5cf9f7c
Dirk Hornung <dirkhornung91@gmail.com>
2019-06-28 11:36:50 +0200

    added defense presentation

--- Plots/plot.py
+++
@@ -1,9 +0,0 @@
-import json
-import matplotlib.pyplot as plt
-
-with open('alephVpA.json') as f:
-    aleph_vpa = json.load(f)['data']
-
-plt.errorbar(aleph_vpa['sbin'], aleph_vpa['sfm2'], yerr=aleph_vpa['derr'], fmt='o', markersize=1)
-plt.ylabel('some numbers')
-plt.show()
