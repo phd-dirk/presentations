dad73a37ee978a004ec0d46f1e0c0e7ba280ec4c
Dirk Hornung <dirkhornung91@gmail.com>
2019-07-16 20:02:12 +0200

    corrected notes up to experimental moment

--- Defence/auto/index.el
+++ Defence/auto/index.el
@@ -2,7 +2,7 @@
  "index"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "utf8") ("mathpazo" "sc" "osf") ("eulervm" "euler-digits" "small") ("biblatex" "style=authortitle-comp" "backend=biber" "style=alphabetic") ("tikz-feynman" "compat=1.1.0")))
+                     '(("inputenc" "utf8") ("mathpazo" "sc" "osf") ("eulervm" "euler-digits" "small") ("biblatex" "style=authortitle-comp" "backend=biber") ("tikz-feynman" "compat=1.1.0")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
